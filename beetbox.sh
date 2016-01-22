3571702083c54247846c1688986f0994604d2dfc
Alexar <hi@alexar.me>
2016-01-22 18:29:41 +1100

    Initial version of beetbox agent which just works :D

---
+++ beetbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# @file beetbox.sh
+#
+# This script faciliates interactions with beetbox vagrant box, a collabrative product of Melbourne Drupal Community.
+#
+# @usage
+#   beetbox		Says hi at the moment!
+
+echo "Hey there!"
